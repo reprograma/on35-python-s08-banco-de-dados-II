b0bd38f32fd56e11faa92bc486a39b765204a4d9
Paloma <palomaavena@gmail.com>
2024-07-25 15:15:37 -0300

    Update exercicio-s08.py

--- exercicios/para-casa/exercicio-s08.py
+++ exercicios/para-casa/exercicio-s08.py
@@ -179,7 +179,18 @@
 # cursor.close()
 # conn.close()
 
+# Extras!!!!
 
+# Tarefas Extras (Opcionais):
 
+# Validação de Dados: Adicione validações para garantir que os dados lidos do CSV sejam válidos 
+# antes de serem inseridos no banco de dados. Por exemplo, verifique se o ano é um número válido, 
+# se o gênero está dentro de uma lista de gêneros permitidos e se o preço é um número válido.
+
+# Tratamento de Erros: Adicione tratamento de exceções para lidar com possíveis erros durante a leitura, 
+# inserção e atualização de dados.
+
+# Interatividade: Torne seus scripts interativos, solicitando entradas do usuário para operações como inserção, 
+# atualização e remoção de dados.
 
 
